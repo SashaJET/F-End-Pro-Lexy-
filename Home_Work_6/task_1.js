f240ae138d4efdf36c015145e8e5394eebfd8b99
Your Name <sashajet.mail@gmail.com>
2019-08-26 00:13:58 +0300

    home work 6

---
+++ Home_Work_6/task_1.js
@@ -0,0 +1,30 @@
+// 1. Даем пользователю ввести число, проверяем что это действительно число.
+// После этого выводим сколько в этом числе четных цифр.
+// Например 1234 должно показать 2, 1111  - ноль
+'use strict';
+let result;
+let userNum;
+
+do {
+	userNum = +prompt('Insert the number', '145899424');
+} while (isNaN(userNum));
+
+userNum = String(userNum);
+
+function getEven(num) {
+	let arrOfNum = [];
+  let count = 0;
+
+	arrOfNum = num.split('');
+
+	for (let i = 0; i < arrOfNum.length; i++){
+		if( (arrOfNum[i] % 2) == 0 ) {
+			count++;
+		}
+	}
+
+	return count;
+}
+
+result = getEven(userNum);
+alert('Tут четных цифр: '+ result);
