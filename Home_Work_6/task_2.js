8f3074972e563b1cf729d1aca67802eb3bde6c14
SashaJET <39765004+SashaJET@users.noreply.github.com>
2019-08-26 15:29:14 +0300

    Update task_2.js

--- Home_Work_6/task_2.js
+++ Home_Work_6/task_2.js
@@ -1,33 +1,38 @@
 // 2. Даем пользователю ввести число от 0 до 10. 
 // С помощью рандома генерируем случайное число. Если введенное пользователем число совпало с нашим, начисляем ему 10 баллов. 
 // После этого спрашиваем хочет ли он продолжать и повторяем с самого начала
-
 'use strict';
 
 const WIN_POINTS = 10;
-let randomNum;
 let score = 0;
 
 function getUserNum() {
-	let userNum;
+  let userNum;
 
-	do {	
-		userNum = +prompt('Guess the number from 0 to 10', '5');
-	} while (isNaN(userNum));
+  do {	
+    userNum = +prompt('Guess the number from 0 to 10', '6');
+  } while (isNaN(userNum) || (userNum < 0 || userNum > 10));
 
-	return userNum;
+  return userNum;
 }
 
-do {
-	randomNum = Math.round( Math.random() * 10 );
-	
-	if ( getUserNum() == randomNum) {		
-		score += WIN_POINTS;
-		alert("You are right");
-	} else {
-		alert("You are wrong");
-	}
+function getRandom(){
+  return Math.round( Math.random() * 10 );
+}
 
+function culcUserScore(){
+  
+  if ( getUserNum() == getRandom() ) {		
+    alert("You are right");
+    return WIN_POINTS;
+  } else {
+    alert("You are wrong");
+    return 0;
+  }
+}
+
+do {	
+  score += culcUserScore();
 } while ( confirm('Do you want to repeat?') );
 
 alert('Your score is: ' + score);
