bd5d7cc4d6925737510b314eedee54ebafdf1c65
SashaJET <39765004+SashaJET@users.noreply.github.com>
2019-09-02 15:06:53 +0300

    fix code

--- Home_Work_8/script.js
+++ Home_Work_8/script.js
@@ -1,4 +1,47 @@
 'user strict'
+// ==============new==============
+const itemsList = document.getElementById('list');
+const count = document.getElementById('count');   
+const addBtn = document.getElementById('addBtn');
+
+addBtn.addEventListener('click', onBtnClick);
+
+function onBtnClick() {
+  let countOfNubs = getCountOfNubs();
+  if (itemsList.innerHTML != ''){
+    cleanList();
+  }
+
+  createElem(countOfNubs);
+}
+
+function getCountOfNubs() {
+   return Number(count.value);
+}
+
+function createElem(num) {    
+  let newLi;
+  for (let i =0; i<num; i++) {  
+    newLi = document.createElement('li');
+    newLi.textContent = (i + 1);
+    itemsList.append(newLi);
+  }
+}
+
+function cleanList() {
+  // if (itemsList.firstChild){
+  //   itemsList.innerHTML == '';
+  // }
+
+  while (itemsList.firstChild) {
+    itemsList.removeChild(itemsList.firstChild);
+
+  }  
+}
+
+
+// ==============old==============
+'user strict'
 const itemsList = document.getElementById('list');
 const count = document.getElementById('count');   
 const addBtn = document.getElementById('addBtn');
