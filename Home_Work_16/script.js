f67f14b48992282abf0e7e7faa7744995280448c
SashaJET <39765004+SashaJET@users.noreply.github.com>
2019-10-28 14:26:42 +0200

    fixed

--- Home_Work_16/script.js
+++ Home_Work_16/script.js
@@ -1,5 +1,6 @@
 'use strict';
-const SHOW_FULL_PHOTO = 'show-full-photo';
+const SHOW_FULL_PHOTO_CLASS = 'show-full-photo';
+const GALLERY_IMG_CLASS = 'gallery-img';
 
 const prom = fetch('https://jsonplaceholder.typicode.com/photos?_limit=50');
 const gallery = document.getElementById('gallery');
@@ -8,14 +9,14 @@ const closeFullPhotoBtn = document.getElementById('close-photo');
 const fullPhotoImgTag = document.getElementById('full-photo');
 const fullPhotoContainer = document.getElementById('full-photo_container');
 
+
 gallery.addEventListener('click', onOpenPhoto);
 closeFullPhotoBtn.addEventListener('click', onBtnClose);
 
 (function getData(){
-  prom.then((resp) => { resp.json().then((data)=> { 
-            const arrOfImg = data;
-            addPhotoToGallery(arrOfImg, gallery);   
-    });
+  prom.then((resp) => { 
+    resp.json()
+    .then(data => addPhotoToGallery(data, gallery));
   });
 }());
 
@@ -28,19 +29,25 @@ function addPhotoToGallery(arr, container){
     });
 }
 
-function toggleState() {
-    fullPhotoContainer.classList.toggle(SHOW_FULL_PHOTO);
-}
-
 function onOpenPhoto(e){
     const fullPhotoUrl = e.target.dataset.photo;
-    
-    fullPhotoImgTag.setAttribute('src', fullPhotoUrl);
-
-    toggleState();
+    if (e.target.classList.contains(GALLERY_IMG_CLASS)){
+        
+        fullPhotoImgTag.setAttribute('src', fullPhotoUrl);
+        showFullPhoto(fullPhotoUrl);   
+    }   
 }
 
 function onBtnClose(){
-    toggleState();    
+    if (fullPhotoContainer.classList.contains(SHOW_FULL_PHOTO_CLASS)){
+        hideFullPhoto();
+    }
 }
 
+function showFullPhoto(){
+    fullPhotoContainer.classList.add(SHOW_FULL_PHOTO_CLASS);
+}
+
+function hideFullPhoto(){
+    fullPhotoContainer.classList.remove(SHOW_FULL_PHOTO_CLASS);
+}
