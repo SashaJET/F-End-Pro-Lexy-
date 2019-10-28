a568b356a14c0bcc4c2d5e27d756b256208cb1bd
SashaJET <39765004+SashaJET@users.noreply.github.com>
2019-10-28 13:06:53 +0200

    done

--- Home_Work_19/script.js
+++ Home_Work_19/script.js
@@ -1,30 +1,77 @@
 'use strict';
 
-const PHOTOS_URL = 'https://jsonplaceholder.typicode.com/photos';
+const PHOTOS_URL = 'https://jsonplaceholder.typicode.com/photos?_limit=100';
 const GALLERY_ITEM_TEMPLATE = document.getElementById('full-photo_template').innerHTML;
 const PAGIN_PAGE_NUM_CLASS = 'pagination-page';
+const PAGINATION_ACTIVE_CLASS = 'active';
+const GALLERY_ITEM_CLASS = 'gallery-img';
+const FULL_IMG_BACKGROUND_CLASS = 'photo_container';
+const SHOW_FULL_PHOTO_CLASS = 'show-full-photo';
 
+const closePhoto = document.getElementById('close-photo');
 const gallery = document.getElementById('gallery');
 const paginationBtn = document.getElementById('pagination');
+const fullPhotoImgTag = document.getElementById('full-photo');
+const fullPhotoContainer = document.getElementById('full-photo_container');
             
-const limitPhotosOnPage = 50;
-let currentPage  ;
+const limitPhotosOnPage = 10;
+let currentPage = 0;
+let totalPages = 0;
 
 init();
 
+paginationBtn.addEventListener('click', onPaginationClick);
+gallery.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(e) {
+    switch(true){
+        case e.target.classList.contains(GALLERY_ITEM_CLASS):
+            showFullPhoto(e.target.dataset.fullPhotoUrl);            
+        break;
+        case e.target.classList.contains(SHOW_FULL_PHOTO_CLASS):
+         console.log(e.target.classList.contains(SHOW_FULL_PHOTO_CLASS))
+            hideFullPhoto();
+        break;
+    }
+}
+
+function onPaginationClick(e){
+    let currentPageBtn = e.target.dataset.count;
+
+    getCurrentPageData(currentPageBtn);
+    setItemsInStorage('currentPage', currentPageBtn);    
+    togglePaginationActiveClass(currentPageBtn);
+}
+
 function init() {
-    
-    currentPage = parseArrFromStorage((getCurrentPageData())) || 1;
+    currentPage = getCurrentPageFromStorage((getCurrentPageData())) || 1;
 
     getListOfPhotos();
     quantityPage()
     getCurrentPageData(currentPage);
 }
 
-
 function requestJson(url){
     return fetch(url)
         .then(resp => resp.json())
+}
+
+function quantityPage(){
+    totalPages =  Math.ceil(parseArrFromStorage().length / limitPhotosOnPage);
+    renderPaginationBtns(totalPages);   
+}
+
+function getCurrentPageFromStorage(){
+    currentPage = JSON.parse(getItemsInStorage('currentPage'));
+    // if(currentPage > totalPages){
+    //     currentPage = 1;
+    // }
+    return currentPage;
+}
+
+function parseArrFromStorage(){
+    const arrOfPhotos = JSON.parse(getItemsInStorage('photos'));    
+    return arrOfPhotos;
 }
 
 function getListOfPhotos(){
@@ -32,76 +79,52 @@ function getListOfPhotos(){
         .then(data => setItemsInStorage('photos', data));
 }
 
-function setItemsInStorage(name, data){
-    // console.log(name)
-    return localStorage.setItem(name, JSON.stringify(data));
-}
-function getItemsInStorage(name){
-    return localStorage.getItem(name);
-}
-
-function parseArrFromStorage(){
-    const arrOfPhotos = JSON.parse(getItemsInStorage('photos'));
-    // console.log(arrOfPhotos)
-    return arrOfPhotos;
-}
-
-function quantityPage(){
-    const numbOfPage =  Math.floor(parseArrFromStorage().length / limitPhotosOnPage); //a так молжно?
-    // console.log(numbOfPage)
-    renderPaginationBtns(numbOfPage);
-}
-
 function renderPaginationBtns(num){
-    let blockCodePgnBtn = '';
-    for(let i = 1; i <= num; i++ ){        
-        blockCodePgnBtn += `<a class="pagination-page"  data-count="${i}">${i}</a>`;
+    let paginBtnHtml = '';
+    for (let i = 1; i <= num; i++ ){        
+        paginBtnHtml += `<a class="pagination-page"  data-count="${i}">${i}</a>`;
     }
-    paginationBtn.innerHTML = blockCodePgnBtn;
+    paginationBtn.innerHTML = paginBtnHtml;
+
+    togglePaginationActiveClass(currentPage);
 }
+
 function getCurrentPageData(numbOfCurrentPage){
-// console.log('numbOfCurrentPage  '+numbOfCurrentPage);
     let photosQuantity = parseArrFromStorage().filter(data =>
         data.id > ((numbOfCurrentPage * limitPhotosOnPage) - limitPhotosOnPage)
-            && data.id < (numbOfCurrentPage*limitPhotosOnPage));
-            // выводит по 49 (limitPhotosOnPage+1)
-    
-            // return photosQuantity;
-            renderPageItems(photosQuantity)
+            && data.id <= (numbOfCurrentPage * limitPhotosOnPage));
+
+            renderPageItems(photosQuantity);
 }
 
 function renderPageItems(list) {
-    // console.log(list)
-
     gallery.innerHTML = list.map(el => {       
         return GALLERY_ITEM_TEMPLATE.replace('{{id}}', el.id)
                                     .replace('{{url}}', el.thumbnailUrl)
                                     .replace('{{fullImgUrl}}', el.url)
     }).join('');
-
-
 }
 
-paginationBtn.addEventListener('click', onPaginationClick);
-
-function onPaginationClick(e){
-    e.preventDefault();
-    let currentPage = e.target.dataset.count;
-    getCurrentPageData(currentPage);
-
-    setItemsInStorage('currentPage', currentPage);
-    
-    if(e.target.classList.contains(PAGIN_PAGE_NUM_CLASS)){      
-        e.target.classList.remove('active');
-        e.target.classList.add('active');
-    }
-    console.log(paginationBtn.children);
-    let l = paginationBtn.children.find(el => el.className ='active');
-    console.log(l);
-    console.log(paginationBtn.children[1].className);
+function showFullPhoto(url){
+    fullPhotoImgTag.setAttribute('src', url);
+    fullPhotoContainer.classList.add(SHOW_FULL_PHOTO_CLASS);
 }
 
-// function setCurrentPagnBtnActive(numberPage){
+function hideFullPhoto(){
+    fullPhotoContainer.classList.add(SHOW_FULL_PHOTO_CLASS);
+}
 
-    
-// }
+function togglePaginationActiveClass(btn) {
+    const activeBtn = pagination.querySelector(`.${PAGINATION_ACTIVE_CLASS}`);
+    activeBtn && activeBtn.classList.remove(PAGINATION_ACTIVE_CLASS);
+
+    paginationBtn.querySelector(`[data-count="${btn}"]`).classList.add(PAGINATION_ACTIVE_CLASS);
+}
+
+function setItemsInStorage(name, data){
+    return localStorage.setItem(name, JSON.stringify(data));
+}
+
+function getItemsInStorage(name){
+    return localStorage.getItem(name);
+}
