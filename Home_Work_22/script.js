ffe27acff83b3a386502929682804febf6814388
SashaJET <39765004+SashaJET@users.noreply.github.com>
2019-11-01 13:23:00 +0200

    22

---
+++ Home_Work_22/script.js
@@ -0,0 +1,103 @@
+$(function(){
+    const $addStickBtn = $('#addStickBtn');
+    const $stickerItemTemplate = $('#sticker_item_template').html();
+    const $form = $('form');
+    const $container = $('#container');
+    const modal = $('#dialog-form').dialog({
+        autoOpen:false,
+        modal:true,
+        buttons: {
+            "Save": addSticker,
+            Cancel: function() {
+                modal.dialog('close');
+            }
+        },
+        close: function() {
+            form[0].reset();
+        }
+    });
+    const form = modal.find('form');
+    let stickerItems = [];
+
+    init();
+
+    $addStickBtn.on('click', onAddStickerBtnClick);
+    form.on('submit', onFormSubmit);
+    $container.on('click', '.sticker-delete-btn', onDeleteStickerBtnClick);
+
+    function onAddStickerBtnClick(){
+        modal.dialog('open');
+    }
+
+    function onFormSubmit(e){
+        e.preventDefault();
+        addSticker();
+    }
+
+    function onDeleteStickerBtnClick(){
+        const $stickerId = $(this).parent().data('stickerId');
+        deleteSticker($stickerId);
+    }
+   
+    function init() {
+        stickerItems = getState();
+        renderStickers(stickerItems);
+    } 
+
+    function renderStickers(stickerItems) {
+        stickerItems.map(addItemInStickerArea);
+    }
+
+    function deleteSticker(id) {
+        removeSticker(id);
+        stickerItems = stickerItems.filter(el => el.id != id);
+        saveState();
+    }
+    function removeSticker(id) {
+        getStickerId(id).remove();
+    }
+
+    function addSticker(){
+        const newSticker = createSticker();
+        $form.serializeArray().map(({name, value}) => newSticker[name] = value);
+        addItemInStickerArea(newSticker)
+        modal.dialog('close');
+        stickerItems.push(stickerItems);
+        // console.log(newSticker);
+        saveState();
+    }
+
+    function addItemInStickerArea(newSticker){
+        $container.append(getStickerItemHtml(newSticker));
+    }
+ 
+    function createSticker() {
+        return {
+            id: Date.now(),
+            title: '',
+            description: ''
+        }
+    }
+
+    function getStickerId(id){
+        return $container.children().filter((el) => $(el).data('stickerId') == id)
+    }
+
+
+    function getStickerItemHtml({id, title, description}){
+        return $stickerItemTemplate.replace('{{id}}', id)
+                                    .replace('{{title}}', title)
+                                    .replace('{{description}}', description);
+    }
+
+    function saveState(){
+        console.log(stickerItems);
+        localStorage.setItem('stickersArea', JSON.stringify(stickerItems));
+    }
+
+    function getState() {
+        const dataFromStorage =  localStorage.getItem('stickersArea');
+        return dataFromStorage ? JSON.parse(dataFromStorage) : [];
+    }
+
+}) // end of jQ
